4488117bedad38ba48573234d6899a9f42758a72
Zach Routhier <zrouthier@gmail.com>
2014-11-01 14:48:29 -0600

    index homepage links all work

--- lib/app.rb
+++ lib/app.rb
@@ -1,11 +1,40 @@
 
-
 class SliceWorksApp < Sinatra::Base
 
   get '/' do
     erb :index
   end
 
+  get '/capitol-hill' do
+    erb :caphill
+  end
 
+  get '/lodo' do
+    erb :lodo
+  end
+
+  get '/menu' do
+    erb :menu
+  end
+
+  get '/catering' do
+    erb :catering
+  end
+
+  get '/locations' do
+    erb :locations
+  end
+
+  get '/catering' do
+    erb :catering
+  end
+
+  get '/gift-cards' do
+    erb :gift_cards
+  end
+
+  get '/contact-us' do
+    erb :contact_us
+  end
 
 end
